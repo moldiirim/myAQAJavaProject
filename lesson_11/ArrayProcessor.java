ade01533a67a8d5919945a55e5c2e395e41fc25c
moldirimbagytzhan <mba6104@gmail.com>
2025-01-18 13:46:02 +0500

    completed lesson 11

---
+++ lesson_11/ArrayProcessor.java
@@ -0,0 +1,25 @@
+package lesson_11;
+
+public class ArrayProcessor {
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4.");
+        }
+
+        int sum = 0;
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+
+        return sum;
+    }
+}
